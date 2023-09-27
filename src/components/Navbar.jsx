0d76907641c0db680e153689a2badd7016d63244
Maycoll Jaramillo Vanegas <maycolljaramillo01@gmail.com>
2023-09-26 20:13:44 -0600

    some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import { styles } from '../style';
+import { navLinks } from '../constants';
+import {logo, menu, close} from '../assets';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <nav
+   className={
+    
+   }>
+
+   </nav>
   )
 }
 
